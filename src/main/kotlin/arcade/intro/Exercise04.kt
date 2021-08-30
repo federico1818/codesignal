0faf64a487fb041ba8f74a7ab56e2fcbe392ae6b
Federico González <federico1818@gmail.com>
2021-08-30 02:50:09 -0300

    arcade - intro - ex 04 - refactor coerceAtLeast

--- src/main/kotlin/arcade/intro/Exercise04.kt
+++ src/main/kotlin/arcade/intro/Exercise04.kt
@@ -4,8 +4,7 @@ class Exercise04 {
     fun adjacentElementsProduct(inputArray: MutableList<Int>): Int {
         var largestProduct: Int = inputArray[0] * inputArray[1]
         for(i in 0 until inputArray.size - 1) {
-            val product: Int = inputArray[i] * inputArray[i + 1]
-            largestProduct = if(product > largestProduct) product else largestProduct
+            largestProduct = largestProduct.coerceAtLeast(inputArray[i] * inputArray[i + 1])
         }
         return largestProduct
     }
